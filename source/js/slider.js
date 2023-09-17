46fec5fdbc82a6255ef269e7a02e6336c36ce1f8
Artur Pletnev <art-pletnev@mail.ru>
2023-09-17 17:02:32 +0300

    Правки

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,4 +1,4 @@
 const container = document.querySelector('.slider');
-document.querySelector('.slider__input').addEventListener('input'), (e) => {
-  container.style.setProperty('--position', '$(e.target.value)%' );
-}
+document.querySelector('.slider__input').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%` );
+})
