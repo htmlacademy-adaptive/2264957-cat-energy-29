ea97d0436096480e371e3e97f802ff306fcdebb8
Artur Pletnev <art-pletnev@mail.ru>
2023-09-19 21:23:51 +0300

    Удаляет build

--- build/js/slider.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector('.slider');
-document.querySelector('.slider__input').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%` );
-})
